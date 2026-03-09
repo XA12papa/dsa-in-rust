fa8580d755ab058350c7530936356bd85574ec80
kala katua <cideshmukhi@gmail.com>
2026-03-09 22:37:56 +0530

    last commit in windows

---
+++ src/Queue/queue.rs
@@ -0,0 +1,34 @@
+use std::{cell::RefCell, collections::{VecDeque, linked_list}, fmt::Debug, rc::Rc};
+
+
+type Link<T> = Option<Rc<RefCell<T>>>;
+
+#[derive(Debug)]
+struct TreeNode<T>{
+    val : T,
+    left : Link<T>,
+    right : Link<T>
+}
+
+#[derive(Debug)]
+struct Tree<T>{
+    head : Link<T>
+}
+
+
+impl <T: Debug> Tree<T>{
+    fn new(&mut self,val : T)->Self {
+        Tree { head: None }
+    }
+
+
+}
+
+
+fn queue_operations(){
+    // let mut q: VecDeque<Link<u32>> = VecDeque::new();
+
+
+
+    
+}
